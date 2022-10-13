9c9f5c74b4ed24f312510ac7adccc848ecda6592
github username <Lukabaramishvili@gmail.com>
2022-10-13 13:11:37 +0400

    add Newsletter & Footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,137 @@
+import {
+  Facebook,
+  Instagram,
+  Pinterest,
+  Twitter,
+  Room,
+  Phone,
+  MailOutline,
+} from '@material-ui/icons';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0px;
+  padding: 0px;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>GROOVE.</Logo>
+        <Description>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et a
+          explicabo saepe voluptatibus repellendus eos? Maiores ut error
+          ducimus. Excepturi iusto harum quo exercitationem similique sequi
+          consequatur quae repellendus rerum.
+        </Description>
+        <SocialContainer>
+          <SocialIcon color='3B5999'>
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color='E4405F'>
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color='55ACEE'>
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color='E60023'>
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Men's Fashion</ListItem>
+          <ListItem>Women's Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: '10px' }} />
+          622 Dixie Path, South Tobinchester 98336
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: '10px' }} />
+          +1 123 456 78
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{ marginRight: '10px' }} />
+          contact@groove.com
+        </ContactItem>
+        <Payment src='https://i.ibb.co/Qfvn4z6/payment.png' />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
