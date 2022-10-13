81e33d6fddf50cc068c17c6316bf6083d92d3626
github username <Lukabaramishvili@gmail.com>
2022-10-13 11:45:12 +0400

    add categories component to show individual shop categories

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -93,8 +93,8 @@ const Slider = () => {
         <ArrowLeftOutlined />
       </Arrow>
       <Wrapper slideIndex={slideIndex}>
-        {sliderItems.map((item) => (
-          <Slide bg={item.bg}>
+        {sliderItems.map((item, id) => (
+          <Slide bg={item.bg} key={id}>
             <ImageContainer>
               <Image src={item.img} />
             </ImageContainer>
