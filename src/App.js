1a5d268f980c2669fd4a55dceadeca3f47009f71
github username <Lukabaramishvili@gmail.com>
2022-10-14 12:38:12 +0400

    add login and register pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from './pages/Home';
+import Login from './pages/Login';
 import Product from './pages/Product';
+import Register from './pages/Register';
 
 const App = () => {
   return (
     <div>
-      <Product />
+      <Login />
     </div>
   );
 };
